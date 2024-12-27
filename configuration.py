e186a2fa08d83f7223021cb2a3a319b73b721810
calint <calin.tenitchi@gmail.com>
2024-12-28 02:18:56 +0700

    .

--- configuration.py
+++ configuration.py
@@ -8,11 +8,11 @@ RAM_ADDRESS_BITWIDTH = 23
 UART_BAUD_RATE = 115200
 # 115200 baud, 8 bits, 1 stop bit, no parity
 
-CACHE_COLUMN_INDEX_BITWIDTH = 4
-# 2^4 = 16 entries (64 B) per cache line
+CACHE_COLUMN_INDEX_BITWIDTH = 3
+# 2 ^ 3 = 8 entries (32 B) per cache line
 
 CACHE_LINE_INDEX_BITWIDTH = 1  # 6
-# 2^6*64 = 4 KB unified instruction and data cache
+# 2 ^ 6 * 32 B = 4 KB unified instruction and data cache
 
 FLASH_TRANSFER_BYTES = 4096  # 0x0020_0000
 # number of bytes to transfer from flash at startup (2 MB)
