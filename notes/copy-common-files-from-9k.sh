362127b32a92f23ec9c548e4891641935cd0e1d4
calint <calin.tenitchi@gmail.com>
2025-01-01 13:35:26 +0700

    .

--- notes/copy-common-files-from-9k.sh
+++ notes/copy-common-files-from-9k.sh
@@ -2,6 +2,7 @@
 set -e
 cd $(dirname "$0")
 
+echo copy common files
 T9KPTH=../../tang-nano-9k--riscv--cache-psram
 T20KPTH=..
 
@@ -20,9 +21,24 @@ rm -rf $T20KPTH/emulator
 rm -rf $T20KPTH/os
 
 cp -ra $T9KPTH/emulator $T20KPTH/
+rm $T20KPTH/emulator/osqa
+
 cp -ra $T9KPTH/os $T20KPTH/
+rm -f $T20KPTH/os/console_application
+rm -f $T20KPTH/os/os
+rm -f $T20KPTH/os/os.bin
+rm -f $T20KPTH/os/os.dat
+rm -f $T20KPTH/os/os.lst
 
 rm -rf $T20KPTH/notes/samples
 cp -ra $T9KPTH/notes/samples $T20KPTH/notes/
 
+echo apply configuraiton
 ../configuration-apply.py
+
+echo make emulator and firmware
+../emulator/make.sh
+../os/make-console-application.sh
+../os/make-fpga-flash-binary.sh
+
+echo done
