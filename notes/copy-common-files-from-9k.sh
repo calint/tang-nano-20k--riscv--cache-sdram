4acc023653eed0f882edf2b08f0f9d9f761b043b
calint <calin.tenitchi@gmail.com>
2025-01-01 11:24:44 +0700

    .

---
+++ notes/copy-common-files-from-9k.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+cd $(dirname "$0")
+
+T9KPTH=../../tang-nano-9k--riscv--cache-psram
+T20KPTH=..
+
+cp -a $T9KPTH/configuration-apply.py $T20KPTH/
+cp -a $T9KPTH/build-using-gowin.sh $T20KPTH/
+cp -a $T9KPTH/program-fpga.sh $T20KPTH/
+cp -a $T9KPTH/make-and-flash-os.sh $T20KPTH/
+cp -a $T9KPTH/flash-fpga.sh $T20KPTH/
+cp -a $T9KPTH/src/core.sv $T20KPTH/src/
+cp -a $T9KPTH/src/registers.sv $T20KPTH/src/
+cp -a $T9KPTH/src/bram.sv $T20KPTH/src/
+cp -a $T9KPTH/src/uarttx.sv $T20KPTH/src/
+cp -a $T9KPTH/src/uartrx.sv $T20KPTH/src/
