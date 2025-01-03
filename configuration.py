476fe66ea566042db03e9410554206cb20eedb3d
calint <calin.tenitchi@gmail.com>
2025-01-03 14:14:49 +0700

    .

--- configuration.py
+++ configuration.py
@@ -59,6 +59,9 @@ BITSTREAM_FLASH_TO_EXTERNAL = 1
 BITSTREAM_FILE_MAX_SIZE_BYTES = 0x0070_0000
 # used to check if the bitstream size is within the allocated space
 
+FIRMWARE_FILE = "os/os.bin"
+# location of the firmware file relative to project root
+
 FIRMWARE_FILE_MAX_SIZE_BYTES = 0x0010_0000
 # used to check if the bitstream size is within the allocated space
 
