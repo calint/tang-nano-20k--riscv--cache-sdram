1d2fd05999e97009d689f3bfd61a796bfaa99f05
calint <calin.tenitchi@gmail.com>
2025-01-09 23:27:07 +0700

    .

--- src/ip/sdram_controller_hs/sdram_controller_hs_tmp.v
+++ src/ip/sdram_controller_hs/sdram_controller_hs_tmp.v
@@ -5,7 +5,7 @@
 //Part Number: GW2AR-LV18QN88C8/I7
 //Device: GW2AR-18
 //Device Version: C
-//Created Time: Thu Jan  9 20:21:08 2025
+//Created Time: Thu Jan  9 23:19:35 2025
 
 //Change the instance name and port connections to the signal names
 //--------Copy here to design--------
