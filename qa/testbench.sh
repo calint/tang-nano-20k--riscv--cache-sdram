37e052490f83c8df7ffd4b26f03848130380487b
calint <calin.tenitchi@gmail.com>
2025-05-08 07:11:47 +0700

    .

--- qa/testbench.sh
+++ qa/testbench.sh
@@ -18,6 +18,7 @@ pwd
 iverilog -g2012 -Winfloop -pfileline=1 -o iverilog.vvp -s testbench \
     ~/apps/gowin/IDE/simlib/gw2a/prim_sim.v \
     $SRCPTH/ip/micron/MT48LC2M32B2.v \
+    $SRCPTH/ip/etron/sdr2mx32.v \
     $SRCPTH/ip/sdram_controller_hs/sdram_controller_hs.vo \
     $SRCPTH/ip/regymm/sd_controller.v \
     $SRCPTH/configuration.sv \
