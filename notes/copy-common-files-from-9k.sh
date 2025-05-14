637573c726589468345fe23b20b20ff50f0e49d6
calint <calin.tenitchi@gmail.com>
2025-05-14 21:27:23 +0700

    .

--- notes/copy-common-files-from-9k.sh
+++ notes/copy-common-files-from-9k.sh
@@ -6,13 +6,6 @@ echo copy common files
 T9KPTH=../../tang-nano-9k--riscv--cache-psram
 T20KPTH=..
 
-cp -a $T9KPTH/scripts/README.md $T20KPTH/scripts/
-cp -a $T9KPTH/configuration-apply.py $T20KPTH/
-cp -a $T9KPTH/scripts/build-using-gowin.sh $T20KPTH/scripts/
-cp -a $T9KPTH/scripts/program-fpga.sh $T20KPTH/scripts/
-cp -a $T9KPTH/scripts/make-and-flash-os.sh $T20KPTH/scripts/
-cp -a $T9KPTH/scripts/flash-fpga.sh $T20KPTH/scripts/
-cp -a $T9KPTH/scripts/tag-and-push.sh $T20KPTH/scripts/
 
 cp -a $T9KPTH/src/core.sv $T20KPTH/src/
 cp -a $T9KPTH/src/registers.sv $T20KPTH/src/
